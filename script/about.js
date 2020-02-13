90d4cec5cfa0af2589bb66eaaac0975eb3c9d71e
Roar-Song <shh331@naver.com>
2020-02-13 13:49:26 +0900

    Fix about.html code

---
+++ script/about.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(window).scroll(function () {
+        var height = $(document).scrollTop();
+        console.log(height);
+    }); 
+    
+    $('.scroll_btn').on('click', () => {
+        
+    });
+});
