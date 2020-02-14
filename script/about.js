d98049f596e0438245d1737ef31d7cd061d60ec1
SongHyeho <shh331@naver.com>
2020-02-14 11:35:59 +0900

    Add about.html content

--- script/about.js
+++ script/about.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
     $(window).scroll(function () {
-        var height = $(document).scrollTop();
-        console.log(height);
+        var screen_h = $(window).height();
+        var scroll_h = $(document).scrollTop();
+        var header_h = $('.main-header').height();
+        if(scroll_h < (screen_h - header_h)) {
+            $('body').removeClass('body-about_white');
+        }
+        else if(scroll_h > (screen_h * 2 - header_h)) {
+            $('body').removeClass('body-about_white');
+        }
+        else if(scroll_h > (screen_h - header_h)) {
+            $('body').addClass('body-about_white');
+        }
     }); 
-    
-    $('.scroll_btn').on('click', () => {
-        
-    });
 });
